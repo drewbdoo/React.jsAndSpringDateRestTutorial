e9d09f405553961f88fe352d01e026b7e833c9a5
Drewbdoo <drewbdoo@gmail.com>
2022-06-03 11:09:25 -0400

    init repo

---
+++ src/main/java/com/example/ReactJS/ReactJsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReactJS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactJsApplication.class, args);
+	}
+
+}
